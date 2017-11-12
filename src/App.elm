95428d070530692060070df439ac489a67032cf5
Martin Boren <martyboren@gmail.com>
2017-11-12 13:22:02 -0800

    Draw image
    just a simple grid of SVG rects for now

--- src/App.elm
+++ src/App.elm
@@ -1,7 +1,15 @@
 module Main exposing (Model, Msg, init, subscriptions, update, view)
 
+import Array exposing (Array)
 import Html exposing (Html, div, text)
+import Html.Attributes
 import Matrix exposing (Matrix)
+import Svg exposing (Svg)
+import Svg.Attributes
+
+
+pxSize =
+    10
 
 
 main : Program Never Model Msg
@@ -46,7 +54,57 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] []
+    let
+        vbWidth =
+            pxSize * Matrix.width model.image
+
+        vbHeight =
+            pxSize * Matrix.height model.image
+    in
+    div
+        []
+        [ Svg.svg
+            [ Svg.Attributes.viewBox
+                ("0 0 " ++ toString vbWidth ++ " " ++ toString vbHeight)
+            , Svg.Attributes.preserveAspectRatio "xMidYMin meet"
+            , Html.Attributes.style
+                [ ( "display", "inline-block" )
+                , ( "position", "absolute" )
+                , ( "top", "0" )
+                , ( "left", "0" )
+                , ( "max-height", "100%" )
+                ]
+            ]
+            (Matrix.toIndexedArray model.image
+                |> Array.toList
+                |> List.map (\( ( col, row ), val ) -> drawCell col row val)
+            )
+        ]
+
+
+drawCell : Int -> Int -> Float -> Svg Msg
+drawCell col row value =
+    let
+        x =
+            col * pxSize
+
+        y =
+            row * pxSize
+
+        fill =
+            if value == 0.0 then
+                "white"
+            else
+                "black"
+    in
+    Svg.rect
+        [ Svg.Attributes.width (toString pxSize)
+        , Svg.Attributes.height (toString pxSize)
+        , Svg.Attributes.x (toString x)
+        , Svg.Attributes.y (toString y)
+        , Svg.Attributes.fill fill
+        ]
+        []
 
 
 subscriptions : Model -> Sub Msg
