ad49f6327fb3ca9311a3f432b054e3b58048ae1a
Martin Boren <martyboren@gmail.com>
2017-11-16 15:30:14 -0800

    Improve CNN architecture
    I used google for efficient hyperparameter selection.
    (ie I took this arch straight from:
    https://github.com/fchollet/keras/blob/master/examples/mnist_cnn.py )

--- backend/train_model.py
+++ backend/train_model.py
@@ -2,7 +2,7 @@
 Train model on MNIST data and save to file
 """
 import keras
-from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D
+from keras.layers import Conv2D, Dense, Dropout, Flatten, MaxPooling2D
 from keras.datasets import mnist
 from keras.models import Sequential
 
@@ -27,18 +27,21 @@ y_test = keras.utils.to_categorical(y_test, 10)
 # Setup model architecture
 
 model = Sequential()
-model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(64, (3,3), activation='relu'))
 model.add(MaxPooling2D((2, 2), 2))
+model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
+              optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
 model.fit(x_train,y_train,
-          epochs=14,
+          epochs=12,
           batch_size=128,
           verbose=2,
           validation_data=(x_test,y_test) )
